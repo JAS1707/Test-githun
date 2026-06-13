0285a50dacd40e50c461e49af3505c3d2d77fdc7
JAS1707 <jaspergoyvaerts5@gmail.com>
2026-06-13 16:32:21 +0200

    first change

--- test/Program.cs
+++ test/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("This is a test program.");
+Console.WriteLine("Have a great day!");
